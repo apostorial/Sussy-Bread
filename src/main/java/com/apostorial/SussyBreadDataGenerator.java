353cb67d2f65702bf35ce7417ac493a9da665c92
Apostorial <t.elouadrassi@gmail.com>
2024-10-26 16:03:04 +0100

    Initial Commit

---
+++ src/main/java/com/apostorial/SussyBreadDataGenerator.java
@@ -0,0 +1,11 @@
+package com.apostorial;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SussyBreadDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
